19b95ccac6309c14cebbaacaa473cd3c01cfa377
Tom Najdek <tom@doppnet.com>
2017-10-25 14:16:08 +0100

    Use rawgit cdn in place of raw.githubusercontent.com. #26

--- src/js/utils.js
+++ src/js/utils.js
@@ -30,7 +30,7 @@ const retrieveStyle = async styleId => {
 	let cacheId = `style-${styleId}`;
 	let style = localStorage.getItem(cacheId);
 	if(!style) {
-		let url = `https://raw.githubusercontent.com/citation-style-language/styles/master/${styleId}.csl`;
+		let url = `https://cdn.rawgit.com/citation-style-language/styles/8470b61e/${styleId}.csl`;
 		let response = await fetch(url);
 		style = await response.text();
 		localStorage.setItem(cacheId, style);
