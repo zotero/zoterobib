41f3c994e0848b3995083facf326e8c9f38fa7f1
Tom Najdek <tom@doppnet.com>
2017-11-15 13:49:12 +0000

    Add support to close Multiple Choice Dialog with Escape. Resolve #57

--- src/js/components/multiple-choice-dialog.jsx
+++ src/js/components/multiple-choice-dialog.jsx
@@ -3,46 +3,57 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const ReactModal = require('react-modal');
+const KeyHandler = require('react-key-handler').default;
+const { KEYDOWN } = require('react-key-handler');
 
 const Button = require('zotero-web-library/lib/component/ui/button');
 
 class MultipleChoiceDialog extends React.Component {
 	render() {
+		//@TODO: Remove wrapping div after zotero/web-library#91
 		return (
-			<ReactModal 
-				isOpen={ this.props.isPickingItem }
-				contentLabel="Please select a citation from the list"
-				className="multiple-choice-dialog modal"
-				overlayClassName="overlay"
-			>
-				<h1 className="title">
-					Please select a citation from the list
-				</h1>
-				<div className="scroll-container">
-					<ul>
-						{
-							this.props.multipleChoiceItems.map(
-								item => {
-									return (
-										<li 
-											className="item" 
-											key={ item.key }
-											onClick={ () => this.props.onMultipleChoiceSelect([item]) }
-										>
-											{ item.value }
-										</li>
-									);
-								}
-							)
-						}
-					</ul>
-				</div>
-				<div className="buttons">
-					<Button onClick={ () => this.props.onMultipleChoiceCancel() }>
-						Cancel
-					</Button>
-				</div>
-			</ReactModal>
+			<div>
+				<KeyHandler 
+					keyEventName={ KEYDOWN }
+					keyValue="Escape"
+					onKeyHandle={ () => this.props.onMultipleChoiceCancel() }
+				/>
+				<ReactModal 
+					isOpen={ this.props.isPickingItem }
+					contentLabel="Please select a citation from the list"
+					className="multiple-choice-dialog modal"
+					overlayClassName="overlay"
+				>
+					
+					<h1 className="title">
+						Please select a citation from the list
+					</h1>
+					<div className="scroll-container">
+						<ul>
+							{
+								this.props.multipleChoiceItems.map(
+									item => {
+										return (
+											<li 
+												className="item" 
+												key={ item.key }
+												onClick={ () => this.props.onMultipleChoiceSelect([item]) }
+											>
+												{ item.value }
+											</li>
+										);
+									}
+								)
+							}
+						</ul>
+					</div>
+					<div className="buttons">
+						<Button onClick={ () => this.props.onMultipleChoiceCancel() }>
+							Cancel
+						</Button>
+					</div>
+				</ReactModal>
+			</div>
 		);
 	}
 
