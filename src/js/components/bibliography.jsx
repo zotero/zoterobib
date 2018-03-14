e44723d957639fa9ed219bc8ed97f1408136e09f
Tom Najdek <tom@doppnet.com>
2018-03-14 17:05:43 +0000

    Use citeproc output as-is in the read-only view. #105

--- src/js/components/bibliography.jsx
+++ src/js/components/bibliography.jsx
@@ -40,16 +40,8 @@ class Bibliography extends React.PureComponent {
 		}
 	}
 
-	render() {
-		if(this.props.bibliography.length === 0) {
-			return null;
-		}
-
-		let html = formatBib(this.props.bibliography);
-		let div = document.createElement('div');
-		div.innerHTML = html;
-
-		let keyHandlers = [
+	get keyHandlers() {
+		return [
 			<KeyHandler
 				key="key-handler-enter"
 				keyEventName={ KEYDOWN }
@@ -63,39 +55,63 @@ class Bibliography extends React.PureComponent {
 				onKeyHandle={ this.handleKeyboard.bind(this) }
 			/>,
 		];
-		return [
-			...keyHandlers,
-			<ul className="bibliography" key="bibliography">
-					{
-						Array.from(div.firstChild.children).map((child, i) => {
-							let [ itemId ] = this.props.bibliography[0]['entry_ids'][i];
-							let { Tag, attrs } = parseTagAndAttrsFromNode(child);
-							return (
-								<li key={ itemId }
-									className="citation"
-									onFocus={ this.handleFocus.bind(this, itemId) }
-									onClick={ () => this.handleEditCitation(itemId) }
-									tabIndex={ 0 }
-								>
-									<div className="csl-entry-container">
-										<Tag
-											dangerouslySetInnerHTML={ { __html: child.innerHTML } }
-											{ ...attrs }
-										/>
-									</div>
-									{
-										!this.props.isReadOnly && (
-											<Button onClick={ this.handleDeleteCitation.bind(this, itemId) }>
-												<Icon type={ '16/remove-sm' } width="16" height="16" />
-											</Button>
-										)
-									}
-								</li>
-							);
-						})
-					}
-			</ul>
-		];
+	}
+
+	renderBibliographyItem(child, i) {
+		let [ itemId ] = this.props.bibliography[0]['entry_ids'][i];
+		let { Tag, attrs } = parseTagAndAttrsFromNode(child);
+		return (
+			<li key={ itemId }
+				className="citation"
+				onFocus={ this.handleFocus.bind(this, itemId) }
+				onClick={ () => this.handleEditCitation(itemId) }
+				tabIndex={ 0 }
+			>
+				<div className="csl-entry-container">
+					<Tag
+						dangerouslySetInnerHTML={ { __html: child.innerHTML } }
+						{ ...attrs }
+					/>
+				</div>
+				{
+					!this.props.isReadOnly && (
+						<Button onClick={ this.handleDeleteCitation.bind(this, itemId) }>
+							<Icon type={ '16/remove-sm' } width="16" height="16" />
+						</Button>
+					)
+				}
+			</li>
+		);
+	}
+
+	render() {
+		if(this.props.bibliography.length === 0) {
+			return null;
+		}
+
+		let html = formatBib(this.props.bibliography);
+		let div = document.createElement('div');
+		div.innerHTML = html;
+
+		if(this.props.isReadOnly) {
+			return [
+				...this.keyHandlers,
+				<div key="bibliography read-only"
+					className="bibliography"
+					dangerouslySetInnerHTML={ { __html: div.innerHTML } }
+				/>
+			];
+		} else {
+			return [
+				...this.keyHandlers,
+				<ul className="bibliography" key="bibliography">
+						{
+							Array.from(div.firstChild.children)
+								.map(this.renderBibliographyItem.bind(this))
+						}
+				</ul>
+			];
+		}
 	}
 
 	static defaultProps = {
