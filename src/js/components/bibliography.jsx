cd011665d7270620b2c5a145584ac9c083d2a550
Flachware <johannes.krtek@flachware.com>
2018-04-27 16:58:14 +0200

    Prepare success state.

--- src/js/components/bibliography.jsx
+++ src/js/components/bibliography.jsx
@@ -84,7 +84,7 @@ class Bibliography extends React.PureComponent {
 					{ content }
 				</div>
 				{ this.props.isAuthorStyle && (
-					<Button className="btn-icon success" onClick={ this.handleCitationCopy.bind(this, rawItem.key) }>
+					<Button className="btn-icon" onClick={ this.handleCitationCopy.bind(this, rawItem.key) }>
 						<Icon type={ '16/copy' } width="16" height="16" />
 						<Icon type={ '16/tick' } width="16" height="16" />
 					</Button>
