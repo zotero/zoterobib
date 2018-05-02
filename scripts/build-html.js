1a2475fe0ccfc08cbdc3d02a9e2d2365565e118c
Tom Najdek <tom@doppnet.com>
2018-05-02 12:02:51 +0100

    Revisit FAQ page. Resolve #191
    * Refactor to avoid inline JS
    * Add header anchors during the build step, rather than at render time
    * Re-use main pages js for installing SmoothScroll. Users coming to the FAQ
      are very likely to have that file already cached and since that file already
      contains SmoothScroll, it's more optimal than creating a dedicated js file
      just for the FAQ page

--- scripts/build-html.js
+++ scripts/build-html.js
@@ -1,34 +1,57 @@
 'use strict';
 
-const fs = require('fs-extra');
-const path = require('path');
+const cheerio = require('cheerio');
 const config = require('config');
-const marked = require('marked');
+const fs = require('fs-extra');
 const Handlebars = require('handlebars');
+const marked = require('marked');
+const path = require('path');
 
 Handlebars.registerHelper('json', context => JSON.stringify(context));
 
-(async () => {
-	// Index
-	var indexConfig = config.get('indexConfig');
-	var srcFile = path.join(__dirname, '..', 'src', 'html', 'index.hbs');
-	var dstFile = path.join(__dirname, '..', 'build', 'index');
-	var index = await fs.readFile(srcFile);
-	var template = Handlebars.compile(index.toString());
-	var output = await template({ indexConfig });
-	await fs.writeFile(dstFile, output);
-	console.log(`index page generated based on ${config.util.getConfigSources().length} config sources`);
+const addAnchors = html => {
+	var $ = cheerio.load(html);
+	var headers = $('h2, h3');
+	headers.map((_, element) => {
+		const id = element.attribs.id;
+		$('<a>')
+			.attr('href', '#' + id)
+			.addClass('anchor-link')
+			.text('#')
+			.appendTo(element);
+	});
+	return $.html();
+};
 
-	// Generate FAQ from Markdown source
-	var faqMarkdown = await fs.readFile(path.join(__dirname, '..', 'src', 'html', 'faq.md'));
-	var faqTemplate = await fs.readFile(path.join(__dirname, '..', 'src', 'html', 'faq.hbs'));
-	dstFile = path.join(__dirname, '..', 'build', 'faq');
-	await fs.ensureDir(path.dirname(dstFile));
-	template = Handlebars.compile(faqTemplate.toString());
-	var faqHTML = marked(faqMarkdown.toString(), { smartypants: true })
+const buildFaqPage = async () => {
+	const faqMarkdown = await fs.readFile(path.join(__dirname, '..', 'src', 'html', 'faq.md'));
+	const faqTemplate = await fs.readFile(path.join(__dirname, '..', 'src', 'html', 'faq.hbs'));
+	const dstFile = path.join(__dirname, '..', 'build', 'faq');
+	const template = Handlebars.compile(faqTemplate.toString());
+	const faqHTML = addAnchors(
+		marked(faqMarkdown.toString(), { smartypants: true })
 		// Remove "-" at end of id attributes, which marked substitutes for question marks
-		.replace(/(id="[^"]+)-"/g, '$1"');
-	output = await template({ faq: faqHTML });
+		.replace(/(id="[^"]+)-"/g, '$1"')
+	);
+	const output = await template({ faq: faqHTML });
 	await fs.writeFile(dstFile, output);
 	console.log('faq page generated');
+};
+
+const buildIndexPage = async () => {
+	const indexConfig = config.get('indexConfig');
+	const srcFile = path.join(__dirname, '..', 'src', 'html', 'index.hbs');
+	const dstFile = path.join(__dirname, '..', 'build', 'index');
+	const index = await fs.readFile(srcFile);
+	const template = Handlebars.compile(index.toString());
+	const output = await template({ indexConfig });
+
+	await fs.writeFile(dstFile, output);
+	console.log(`index page generated based on ${config.util.getConfigSources().length} config sources`);
+};
+
+(async () => {
+	const dstDir = path.join(__dirname, '..', 'build');
+	await fs.ensureDir(dstDir);
+	await Promise.all([buildIndexPage(), buildFaqPage()]);
 })();
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const SmoothScroll = require('smooth-scroll');
 const ZoteroBibComponent = require('./bib-component');
-const targetDom = document.getElementById('zotero-bib');
-const config = JSON.parse(document.getElementById('zotero-bib-config').textContent);
 
-ZoteroBibComponent.init(targetDom, config);
+const targetDom = document.getElementById('zotero-bib');
+
+if(targetDom) {
+	const config = JSON.parse(document.getElementById('zotero-bib-config').textContent);
+	ZoteroBibComponent.init(targetDom, config);
+} else {
+	new SmoothScroll('main.faq a[href*="#"]', {offset: 16});
+}
