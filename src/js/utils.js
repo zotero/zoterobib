960e8d930afccb0420c7e646ca5e69e886903b06
Tom Najdek <tom@doppnet.com>
2018-05-23 22:24:15 +0100

    For failed locale requests, return false. Fix #236

--- src/js/utils.js
+++ src/js/utils.js
@@ -63,7 +63,11 @@ const syncRequestAsText = url => {
 	let xhr = new XMLHttpRequest();
 	xhr.open('GET', url, false);
 	xhr.send();
-	return xhr.responseText;
+	if(xhr.readyState === xhr.DONE && xhr.status === 200) {
+		return xhr.responseText;
+	} else {
+		return false;
+	}
 };
 
 const parseIdentifier = identifier => {
