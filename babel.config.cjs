ffd03bf88835e80c8b002e76051e7156caf5f312
Tom Najdek <tom@doppnet.com>
2021-05-24 19:38:16 +0200

    Update React to 17.x

--- babel.config.cjs
+++ babel.config.cjs
@@ -5,7 +5,9 @@ const presets = [
 
 const plugins = [
 	["@babel/plugin-proposal-decorators", { "legacy": true }],
-	["@babel/plugin-proposal-class-properties", { "loose" : true }]
+	["@babel/plugin-proposal-class-properties", { "loose" : true }],
+	["@babel/plugin-proposal-private-methods", { "loose": true }]
+
 ];
 
 module.exports = { presets, plugins };
