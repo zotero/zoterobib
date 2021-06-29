8c7a7b276d0b7fcd991aedcb1dc6cc2f963d8404
Tom Najdek <tom@doppnet.com>
2021-06-29 12:56:42 +0200

    Optimise and Refactor MultipleChoiceDialog

--- src/js/components/multiple-choice-dialog.jsx
+++ src/js/components/multiple-choice-dialog.jsx
@@ -1,158 +1,157 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import { default as KeyHandler } from 'react-key-handler';
-import { KEYDOWN } from 'react-key-handler';
+import React, { useCallback, useRef, memo } from 'react';
+import { default as KeyHandler, KEYDOWN } from 'react-key-handler';
+
 import Button from './ui/button';
-import Spinner from './ui/spinner';
 import Icon from './ui/icon';
 import Modal from './modal';
+import Spinner from './ui/spinner';
 
-class MultipleChoiceDialog extends React.Component {
-	handleSelect(item = this.focusedItem) {
-		if(item) {
-			this.props.onMultipleChoiceSelect(item);
+const ChoiceItem = memo(({ item, onFocus, onClick }) => {
+	let badge = null;
+	let title = item.value.title || '';
+
+	if(item.source === 'url') {
+		let badges = [];
+		let matches = title.match(/^\[([A-Z]*)\]/);
+		while(matches) {
+			const badge = matches[1]
+				.split(' ')
+				.map(w => w.substring(0, 1).toUpperCase() + w.substring(1).toLowerCase())
+				.join(' ');
+			badges.push(badge);
+			title = title.substring(matches[0].length);
+			matches = title.match(/^\[([A-Z]*)\]/);
 		}
-	}
-
-	handleFocus(item) {
-		this.focusedItem = item;
-	}
-
-	handleCancel() {
-		this.props.onMultipleChoiceCancel();
-	}
-
-	handleMore() {
-		this.props.onMultipleChoiceMore();
-	}
-
-	renderItem(item) {
-		let badge = null;
-		let title = item.value.title || '';
-		if(item.source === 'url') {
-			let badges = [];
-			let matches = title.match(/^\[([A-Z]*)\]/);
-			while(matches) {
-				let badge = matches[1]
-					.split(' ')
-					.map(w => w.substring(0, 1).toUpperCase() + w.substring(1).toLowerCase())
-					.join(' ');
-				badges.push(badge);
-				title = title.substring(matches[0].length);
-				matches = title.match(/^\[([A-Z]*)\]/);
-			}
-			badges = [ ...new Set(badges) ].filter(b => b.length > 1);
-			if(badges.length) {
-				badge = badges[0];
-			}
-		} else if(item.value.itemType) {
-			badge = item.value.itemType;
+		badges = [ ...new Set(badges) ].filter(b => b.length > 1);
+		if(badges.length) {
+			badge = badges[0];
 		}
-		return (
-			<li
-				className="result"
-				key={ item.signature }
-				onFocus={ this.handleFocus.bind(this, item) }
-				onClick={ this.handleSelect.bind(this, item) }
-				tabIndex={ 0 }
-			>
-				{ badge && <span key={badge} className="badge badge-light d-sm-none">{ badge }</span> }
-				<h5 className="title">
-					<span className="title-container">
-						{ title }
-					</span>
-					{ badge && <span key={badge} className="badge badge-light d-xs-none d-sm-inline-block">{ badge }</span> }
-				</h5>
-				{ item.value.description && (
-					<p className="description">
-						{item.value.description}
-					</p>
-				)}
-			</li>
-		);
+	} else if(item.value.itemType) {
+		badge = item.value.itemType;
 	}
 
-	renderMoreSection() {
-		const { isTranslatingMore, moreItemsLink } = this.props;
-		if(isTranslatingMore) {
-			return <Spinner />;
-		} else if(moreItemsLink !== null) {
-			return (
-				<Button
-					className="btn-outline-secondary btn-min-width"
-					onClick={ this.handleMore.bind(this) }
-				>
-					More…
-				</Button>
-			);
-		}
-	}
+	return (
+		<li
+			className="result"
+			data-signature={ item.signature }
+			onFocus={ onFocus }
+			onClick={ onClick }
+			tabIndex={ 0 }
+		>
+			{ badge && <span className="badge badge-light d-sm-none">{ badge }</span> }
+			<h5 className="title">
+				<span className="title-container">
+					{ title }
+				</span>
+				{ badge && <span className="badge badge-light d-xs-none d-sm-inline-block">{ badge }</span> }
+			</h5>
+			{ item.value.description && (
+				<p className="description">
+					{item.value.description}
+				</p>
+			)}
+		</li>
+	);
+});
 
-	render() {
-		if(!this.props.multipleChoiceItems || this.props.activeDialog !== 'MULTIPLE_CHOICE_DIALOG') {
-			return null
-		}
-
-		return (
-			<Modal
-				isOpen={ this.props.activeDialog === 'MULTIPLE_CHOICE_DIALOG' }
-				contentLabel="Select the entry to add:"
-				className="multiple-choice-dialog modal modal-lg"
-				onRequestClose={ this.handleCancel.bind(this) }
-			>
-				<KeyHandler
-					keyEventName={ KEYDOWN }
-					keyValue="Escape"
-					onKeyHandle={ this.handleCancel.bind(this) }
-				/>
-				<KeyHandler
-					keyEventName={ KEYDOWN }
-					keyValue="Enter"
-					onKeyHandle={ this.handleSelect.bind(this, undefined) }
-				/>
-				<div className="modal-content" tabIndex={ -1 }>
-					<div className="modal-header">
-						<h4 className="modal-title text-truncate">
-							Please select a citation from the list
-						</h4>
-						<Button
-							icon
-							className="close"
-							onClick={ this.handleCancel.bind(this) }
-						>
-							<Icon type={ '24/remove' } width="24" height="24" />
-						</Button>
-					</div>
-					<div className="modal-body">
-						<ul className="results">
-							{ this.props.multipleChoiceItems.map(
-								this.renderItem.bind(this)
-							) }
-						</ul>
-						{ this.props.moreItemsLink && (
-							<div className="more-items-action">
-								{ this.renderMoreSection() }
-							</div>
-						)}
-					</div>
-				</div>
-			</Modal>
-		);
-	}
-
-	static defaultProps = {
-		multipleChoiceItems: []
-	}
-
-	static propTypes = {
-		activeDialog: PropTypes.string,
-		isTranslatingMore: PropTypes.bool,
-		moreItemsLink: PropTypes.object,
-		multipleChoiceItems: PropTypes.array,
-		onMultipleChoiceCancel: PropTypes.func.isRequired,
-		onMultipleChoiceMore: PropTypes.func.isRequired,
-		onMultipleChoiceSelect: PropTypes.func.isRequired,
-	}
+ChoiceItem.propTypes = {
+	item: PropTypes.object,
+	onClick: PropTypes.func,
+	onFocus: PropTypes.func,
 }
 
-export default MultipleChoiceDialog;
+ChoiceItem.displayName = 'ChoiceItem';
+
+const getItem = (ev, items) => items.find(item => item.signature === ev.currentTarget.closest('[data-signature]')?.dataset.signature);
+
+const MultipleChoiceDialog = props => {
+	const { activeDialog, isTranslatingMore, moreItemsLink, multipleChoiceItems,
+	onMultipleChoiceCancel, onMultipleChoiceMore, onMultipleChoiceSelect } = props;
+	const focusedItem = useRef(null);
+
+	const handleItemClick = useCallback(ev => {
+		const item = getItem(ev, multipleChoiceItems);
+		onMultipleChoiceSelect(item);
+	}, [multipleChoiceItems, onMultipleChoiceSelect]);
+
+	const handleItemFocus = useCallback(ev => {
+		focusedItem.current = getItem(ev, multipleChoiceItems);
+	}, [multipleChoiceItems]);
+
+	const handleKeyboardSelect = useCallback(() => {
+		onMultipleChoiceSelect(focusedItem.current);
+	}, [onMultipleChoiceSelect]);
+
+	return (multipleChoiceItems && activeDialog === 'MULTIPLE_CHOICE_DIALOG') ? (
+		<Modal
+			isOpen={ activeDialog === 'MULTIPLE_CHOICE_DIALOG' }
+			contentLabel="Select the entry to add:"
+			className="multiple-choice-dialog modal modal-lg"
+			onRequestClose={ onMultipleChoiceCancel }
+		>
+			<KeyHandler
+				keyEventName={ KEYDOWN }
+				keyValue="Escape"
+				onKeyHandle={ onMultipleChoiceCancel }
+			/>
+			<KeyHandler
+				keyEventName={ KEYDOWN }
+				keyValue="Enter"
+				onKeyHandle={ handleKeyboardSelect }
+			/>
+			<div className="modal-content" tabIndex={ -1 }>
+				<div className="modal-header">
+					<h4 className="modal-title text-truncate">
+						Please select a citation from the list
+					</h4>
+					<Button
+						icon
+						className="close"
+						onClick={ onMultipleChoiceCancel }
+					>
+						<Icon type={ '24/remove' } width="24" height="24" />
+					</Button>
+				</div>
+				<div className="modal-body">
+					<ul className="results">
+						{ multipleChoiceItems.map(item => (
+							<ChoiceItem
+								key={ item.signature }
+								item={ item }
+								onFocus={ handleItemFocus }
+								onClick={ handleItemClick }
+							/>
+						)) }
+					</ul>
+					{ moreItemsLink && (
+						<div className="more-items-action">
+							{ isTranslatingMore ? <Spinner /> : (
+								moreItemsLink !== null && (
+									<Button
+										className="btn-outline-secondary btn-min-width"
+										onClick={ onMultipleChoiceMore }
+									>
+										More…
+									</Button>
+								)
+							) }
+						</div>
+					) }
+				</div>
+			</div>
+		</Modal>
+	) : null;
+}
+
+MultipleChoiceDialog.propTypes = {
+	activeDialog: PropTypes.string,
+	isTranslatingMore: PropTypes.bool,
+	moreItemsLink: PropTypes.object,
+	multipleChoiceItems: PropTypes.array,
+	onMultipleChoiceCancel: PropTypes.func.isRequired,
+	onMultipleChoiceMore: PropTypes.func.isRequired,
+	onMultipleChoiceSelect: PropTypes.func.isRequired,
+}
+
+export default memo(MultipleChoiceDialog);
