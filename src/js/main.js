aab6f2be4ba92e65a1bbf3dd7fcd6f896e332a43
Tom Najdek <tom@doppnet.com>
2022-07-22 12:14:07 +0200

    Fix missing string in Safari <= 12
    "10,000+ other styles available" string in style selector was displaying
    id instead of a message because of missing polyfills

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,13 @@ import './wdyr';
 import SmoothScroll from 'smooth-scroll';
 import ZoteroBibComponent from './bib-component';
 
+// required for Safari 10.1
+import '@formatjs/intl-getcanonicallocales/polyfill'
+import '@formatjs/intl-locale/polyfill'
+// required for Safari <= 12.1
+import '@formatjs/intl-pluralrules/polyfill'
+import '@formatjs/intl-pluralrules/locale-data/en' // locale-data for en
+
 const targetDom = document.getElementById('zotero-bib');
 
 if(targetDom) {
